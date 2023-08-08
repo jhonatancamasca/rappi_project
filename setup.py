c16c166f812044db6e3ad0b4968d740743e77e54
jcamascah@gmail.com <jcamascah@gmail.com>
2023-08-08 13:28:28 -0500

    adding project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Rappi Challenge Deployment',
+    author='Jhonatan Camasca',
+    license='',
+)
