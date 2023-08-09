f4f3e0350d0ac0addcb48668d634cb69b6c713eb
jcamascah@gmail.com <jcamascah@gmail.com>
2023-08-09 00:07:12 -0500

    adding changes in the DockeFile

--- src/app.py
+++ src/app.py
@@ -5,15 +5,11 @@ import pandas as pd
 import re
 import numpy as np
 from features.build_features import FeatureEngineering,DataProcessing, get_class
+from pathlib import Path
 
-
-
-
-path='C:/Users/dvice/Desktop/daata/orders.csv'
-#df=pd.read_csv(path)
 
 # Load the trained model and data transformation pipeline
-with open('C:/Users/dvice/Desktop/daata/Rappi_Project/rappi_challenge/models/model.pkl', 'rb') as model_file:
+with open(Path('../models/model.pkl'), 'rb') as model_file:
     model = pickle.load(model_file)
 
 
@@ -23,7 +19,7 @@ st.write("Upload a CSV file:")
 uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
 
 if uploaded_file is not None:
-    df = pd.read_csv(path)
+    df = pd.read_csv(uploaded_file)
     data_processing = DataProcessing()
     processed_data = data_processing.process_data(df)
     prediction = model.predict(processed_data.values)
